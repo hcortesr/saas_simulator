6964036861166811a0c9440834de0eca2b322507
Soy Hector <heccorram@gmail.com>
2025-02-14 15:11:01 -0600

    def

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://hcortesr.github.io/react-first-app",
+  base: "https://hcortesr.github.io/saas_simulator",
 })
